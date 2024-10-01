2a2cfb846728e847d5927ce3478d21f0f5ba6ff4
PaulinSlatt <pymoreno0211@gmail.com>
2024-10-01 18:49:55 -0300

    section de tratamentos finalizada

---
+++ src/app/componentes/nossos-servicos/nossos-servicos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nossos-servicos',
+  standalone: true,
+  imports: [],
+  templateUrl: './nossos-servicos.component.html',
+  styleUrl: './nossos-servicos.component.css'
+})
+export class NossosServicosComponent {
+
+}
