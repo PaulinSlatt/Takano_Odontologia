460a772c755d29c66e1f11f74bf320b7180547d3
PaulinSlatt <pymoreno0211@gmail.com>
2024-10-18 03:34:30 -0300

    adicionado novo componente fale conosco, faltando integracao com api maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,14 @@ import { AgendeConsultaComponent } from './componentes/agende-consulta/agende-co
 import { QuemSomosComponent } from './componentes/quem-somos/quem-somos.component';
 import { NossosServicosComponent } from './componentes/nossos-servicos/nossos-servicos.component';
 import { DentistasComponent } from "./componentes/dentistas/dentistas.component";
+import { FaleConoscoComponent } from "./componentes/fale-conosco/fale-conosco.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AgendeConsultaComponent, QuemSomosComponent, NossosServicosComponent, DentistasComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AgendeConsultaComponent, QuemSomosComponent, NossosServicosComponent, DentistasComponent, FaleConoscoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/fale-conosco/fale-conosco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fale-conosco',
+  standalone: true,
+  imports: [],
+  templateUrl: './fale-conosco.component.html',
+  styleUrl: './fale-conosco.component.css'
+})
+export class FaleConoscoComponent {
+
+}
