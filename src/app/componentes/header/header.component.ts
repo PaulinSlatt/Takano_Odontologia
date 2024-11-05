19375b9c7dab8c929e4b0816bfd0bfd0b3af919e
PaulinSlatt <pymoreno0211@gmail.com>
2024-11-05 14:54:08 -0300

    scroll mais cadenciado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -10,9 +10,36 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   
   scrollToSection(section: string) {
-    const element = document.getElementById(section);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+    const target = document.getElementById(section);
+    if (target) {
+      this.smoothScrollTo(target);
     }
   }
+
+  private smoothScrollTo(element: HTMLElement) {
+    const startPosition = window.pageYOffset;
+    const targetPosition = element.getBoundingClientRect().top + startPosition;
+    const duration = 2000; 
+    let startTime: number | null = null;
+
+    function animation(currentTime: number) {
+      if (!startTime) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition - startPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t: number, b: number, c: number, d: number) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+
+
+
 }
