cf53ce58ae73d941177c57b49d84e33e60971893
Kelvin Marques Cordeiro <kelvin_marques@outlook.com>
2025-02-08 15:46:57 -0300

    Integrado backend com front pós ajuste de responsividade

--- backend/TakanoOdontologia/formulario/views.py
+++ backend/TakanoOdontologia/formulario/views.py
@@ -28,10 +28,10 @@ def enviar_agenda(request):
                 tipo_consulta=data.get('tipo_consulta'),
                 horario_preferencia=data.get('horario_preferencia'),
             )
-            
+            print(contato)  # Log para verificar se o registro foi criado
             # Envio de e-mail
-            enviar_email(contato)
-
+            log_envio_email = enviar_email(contato)
+            print(log_envio_email)  # Log para verificar se o e-mail foi enviado
             return JsonResponse({'message': 'Consulta agendada com sucesso!'}, status=201)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
@@ -41,20 +41,30 @@ def enviar_agenda(request):
 def enviar_email(contato):
     assunto = "Confirmação de Agendamento"
     mensagem_html = render_to_string('formulario/email_template.html', {'contato': contato})
+    
+    # Verificando se os parâmetros obrigatórios não são None
+    if not all([assunto, mensagem_html, contato.email]):
+        print("Erro: Um ou mais parâmetros obrigatórios estão indefinidos.")
+        return "Erro: Parâmetros obrigatórios indefinidos."
 
-    send_mail(
-        assunto,
-        f"""Cliente {contato.nome} solicitou um agendamento!
-
-        Nome: {contato.nome},
-        E-mail: {contato.email},
-        Telefone: {contato.phone_number},
-        Primeira Consulta?  {'Sim' if contato.primeira_consulta else 'Não'}
-        Tipo de consulta: {contato.tipo_consulta}
-        Horário preferido: {contato.horario_preferencia}
-        """,
-        'EmailEnvio@gmail.com',
-        ['secretaria@takanoOdontologia.com'],  # Ajuste com o e-mail real do cliente
-        fail_silently=False,
-        html_message=mensagem_html 
-    )
+    try:
+        retorno = send_mail(
+            assunto,
+            f"""Cliente {contato.nome} solicitou um agendamento!
+            Nome: {contato.nome},
+            E-mail: {contato.email},
+            Telefone: {contato.phone_number},
+            Primeira Consulta?  {'Sim' if contato.primeira_consulta else 'Não'}
+            Tipo de consulta: {contato.tipo_consulta}
+            Horário preferido: {contato.horario_preferencia}
+            """,
+            'no-reply@example.com',
+            ['secretaria@takanoOdontologia.com'],  # Ajuste com o e-mail real do cliente
+            fail_silently=True,
+            html_message=mensagem_html 
+            )
+        return "E-mail enviado com sucesso!"
+    except Exception as e:
+        print('Erro ao enviar e-mail:', str(e))
+        return "Erro ao enviar e-mail"
+    
